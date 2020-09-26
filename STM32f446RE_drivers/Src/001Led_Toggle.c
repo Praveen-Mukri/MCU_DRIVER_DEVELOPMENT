3f7ce8dc944e089dbd6ab18e3177ee903ad0726c
Pravee Mukri <praveensmukri@gmail.com>
2020-09-26 23:59:22 +0530

    Added toggling a LED on button press application

--- STM32f446RE_drivers/Src/001Led_Toggle.c
+++ STM32f446RE_drivers/Src/001Led_Toggle.c
@@ -16,21 +16,33 @@ void delay(void)
 
 int main(void)
 {
-    GPIO_Handle_t GpioLed;
+    GPIO_Handle_t GpioLed1;
+    GPIO_Handle_t GpioLed2;
 
-    GpioLed.pGPIOx = GPIOA;
-    GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_6;
-    GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
-    GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
-    GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
-    GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+    // Push-Pull Configuration
+    GpioLed1.pGPIOx = GPIOA;
+    GpioLed1.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_6;
+    GpioLed1.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+    GpioLed1.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+    GpioLed1.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+    GpioLed1.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+    // Open-Drain Configuration ( intensity will be very low)
+    GpioLed2.pGPIOx = GPIOA;
+    GpioLed2.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_5; // PA5 on board Green LED
+    GpioLed2.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+    GpioLed2.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+    GpioLed2.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_OD;
+    GpioLed2.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PIN_PU;
 
     GPIO_PeriClockControl(GPIOA, ENABLE);
-    GPIO_Init(&GpioLed);
+    GPIO_Init(&GpioLed1);
+    GPIO_Init(&GpioLed2);
 
     while(1)
     {
         GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_6);
+        GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
         delay();
     }
     return 0;
